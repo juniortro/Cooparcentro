ab92957d7f0e8e84bd2a5170854b6e6e95e6c34c
Carlos Junior <junior_tro@yahoo.com.br>
2020-12-06 00:34:21 -0300

    fix responsive bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,36 +9,48 @@ import '../styles/footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <div className="footer">
-                <img src={Logo} alt="Logo" />
-                <div className="mapa">
-                <h2>MAPA DO SITE</h2>
-                    <ul>
-                        <li><a href="#home">HOME</a></li>
-                        <li><a href="#quemsomos">QUEM SOMOS</a></li>
-                        <li><a href="#servicos">SERVIÇOS</a></li>
-                        <li><a href="#noticias">NOTÍCIAS</a></li>
-                        <li><a href="#eventos">EVENTOS</a></li>
-                        <li><a href="#faleconosco">FALE CONOSCO</a></li>
-                        <li><a href="#trabalheconosco">TRABALHE CONOSCO</a></li>
-                    </ul>
-                </div>
+        <div className="footer">
 
-                <div className="midias">
-                    <h2>MÍDIAS SOCIAIS</h2>
+            <div className="logo-footer">
+                <img src={Logo} alt="Logo" />
+            </div>
+
+            <div className="mapa">
+                <h2>MAPA DO SITE</h2>
+                <ul>
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="#quemsomos">QUEM SOMOS</a></li>
+                    <li><a href="#servicos">SERVIÇOS</a></li>
+                    <li><a href="#noticias">NOTÍCIAS</a></li>
+                    <li><a href="#eventos">EVENTOS</a></li>
+                    <li><a href="#faleconosco">FALE CONOSCO</a></li>
+                    <li><a href="#trabalheconosco">TRABALHE CONOSCO</a></li>
+                </ul>
+            </div>
+
+            <div className="midias">
+                <h2>MÍDIAS SOCIAIS</h2>
+                <div className="footer-icons">
                     <img src={Facebook} alt="facebook" /> <p>@COOPARCENTRO</p>
+                </div>
+                <div className="footer-icons">
                     <img src={Instagram} alt="instagram" /> <p>@COOPARCENTRO</p>
+                </div>
+                <div className="footer-icons">
                     <img src={Youtube} alt="youtube" /> <p>@COOPARCENTRO</p>
                 </div>
+            </div>
 
-                <div className="contatos">
-                    <h2>CONTATOS</h2>
+            <div className="footer-contacts">
+                <h2>CONTATOS</h2>
+                <div className="footer-icons">
                     <img src={Phone} alt="fone" /> <p>(55) 3211-1122</p>
+                </div>
+                <div className="footer-icons">
                     <img src={Location} alt="localizacao" /> <p>ROD. BR-392, KM. 348 97070-160, SANTA MARIA/RS</p>
                 </div>
             </div>
-            
+
             <div className="endpage">
                 <p>© TODOS DIREITOS RESERVADOS. COOPARCENTRO 2020.</p>
             </div>
